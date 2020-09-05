c8c8a0c2e24c9996db529c3a67637fc42dce505a
Ananda Umamil <zweimach@zweimach.org>
2020-09-05 18:27:57 +0700

    Add Procfile and enable CORS

--- server/src/index.js
+++ server/src/index.js
@@ -4,5 +4,6 @@ import { AppModule } from "./app.module";
 
 (async function () {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT);
 })();
